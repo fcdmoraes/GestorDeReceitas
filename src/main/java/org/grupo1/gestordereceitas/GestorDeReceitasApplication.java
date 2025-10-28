0d8c11d4c2aab9ac95b2269b5e66d3f12f720974
fcdmoraes <ffllaa@gmail.com>
2025-10-27 21:53:07 -0300

    Criação do projeto spring boot para gestão de receitas

---
+++ src/main/java/org/grupo1/gestordereceitas/GestorDeReceitasApplication.java
@@ -0,0 +1,13 @@
+package org.grupo1.gestordereceitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorDeReceitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorDeReceitasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/grupo1/gestordereceitas/GestorDeReceitasApplicationTests.java
@@ -0,0 +1,13 @@
+package org.grupo1.gestordereceitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorDeReceitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
